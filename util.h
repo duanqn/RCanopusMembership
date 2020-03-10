cb2afe3a889ff1215962716e8fc30ccdacbed9e4
Qingnan Duan <duanqn_own_1@yeah.net>
2020-03-10 17:17:09 -0400

    Add async helpers

--- util.h
+++ util.h
@@ -1,7 +1,11 @@
 #ifndef UTIL_H_
 #define UTIL_H_
 
+#include <type_traits>
 #include <functional>
+#include <chrono>
+#include <thread>
+#include <future>
 
 namespace AlgoLib{
 namespace Util{
@@ -21,6 +25,83 @@ namespace Util{
 
         ~TCleanup(){
             m_func();
+        }
+    };
+
+    template<class Function, class... Args>
+    class DelayedAsyncExecution{
+        private:
+        using return_type=std::invoke_result_t<Function, Args...>;
+        std::future<return_type> m_result;
+        std::thread *pStdThread;
+
+        template <class T>
+        std::decay_t<T> decay_copy(T&& v) { return std::forward<T>(v); }
+
+        public:
+        explicit DelayedAsyncExecution(std::chrono::milliseconds const& sleep_ms, Function&& func, Args&&... args){
+            std::packaged_task<return_type(Args...)> task([this, &func, &sleep_ms](Args... args){
+                std::this_thread::sleep_for(sleep_ms);
+                return func(args...);
+            });
+            m_result = task.get_future();
+
+            pStdThread = new std::thread(std::move(task), args...);
+        }
+
+        return_type waitForResult(){
+            if(pStdThread != nullptr){
+                if(pStdThread->joinable()){
+                    pStdThread->join();
+                }
+            }
+            return m_result.get();
+        }
+
+        ~DelayedAsyncExecution(){
+            if(pStdThread != nullptr){
+                if(pStdThread->joinable()){
+                    pStdThread->join();
+                }
+                delete pStdThread;
+            }
+        }
+    };
+
+    template<class Function, class... Args>
+    class AsyncExecution{
+        private:
+        using return_type=std::invoke_result_t<Function, Args...>;
+        std::future<return_type> m_result;
+        std::thread *pStdThread;
+
+        template <class T>
+        std::decay_t<T> decay_copy(T&& v) { return std::forward<T>(v); }
+
+        public:
+        explicit AsyncExecution(Function&& func, Args&&... args){
+            std::packaged_task<return_type(Args...)> task(func);
+            m_result = task.get_future();
+
+            pStdThread = new std::thread(std::move(task), args...);
+        }
+
+        return_type waitForResult(){
+            if(pStdThread != nullptr){
+                if(pStdThread->joinable()){
+                    pStdThread->join();
+                }
+            }
+            return m_result.get();
+        }
+
+        ~AsyncExecution(){
+            if(pStdThread != nullptr){
+                if(pStdThread->joinable()){
+                    pStdThread->join();
+                }
+                delete pStdThread;
+            }
         }
     };
 }
--- util_test.cpp
+++ util_test.cpp
@@ -1,13 +1,32 @@
 #include <cstdio>
+#include <chrono>
+#include <type_traits>
 #include "util.h"
 
+int func(int index){
+    printf("In func %d!\n", index);
+    return 0;
+}
+
 int main(){
-    int i = 5;
+    int i = 0;
 
     AlgoLib::Util::TCleanup t([&](){
-        printf("%d\n", i);
+        printf("Cleaning up!\n");
+        printf("i = %d\n", i);
     });
 
-    printf("Hello, world!\n");
+    AlgoLib::Util::DelayedAsyncExecution job(std::chrono::milliseconds(2000), func, 5);
+
+    for(; i < 10; ++i){
+        for(int j = 0; j < 10000; ++j){
+            for(int k = 0; k < 10000; ++k){}
+        }
+        printf("Hello, world!\n");
+    }
+
+    int result = job.waitForResult();
+
+    printf("Async result = %d\n", result);
     return 0;
 }

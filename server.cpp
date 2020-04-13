d1a3e395a6c55268bb81f61d48d4b60cd899b7d1
Qingnan Duan <duanqn_own_1@yeah.net>
2020-04-13 02:06:17 -0400

    Start listener, sender and dummy dispatcher

--- server.cpp
+++ server.cpp
@@ -4,6 +4,7 @@
 #include "util.h"
 #include "ConnManager.h"
 #include <memory>
+#include "QueueElement.h"
 
 int main(){
     Config config;
@@ -28,5 +29,19 @@ int main(){
             return 1;
         }
     }
+
+    std::unique_ptr<ConnManager> upMgr;
+    printf("Setting up connections...\n");
+    try{
+        upMgr = std::make_unique<ConnManager>(config);
+    }
+    catch(Exception &e){
+        printf("Exception code %X\n", e.getReason());
+        throw e;
+    }
+    
+    printf("Connection set up successfully.\n");
+
+    upMgr->start();
     return 0;
 }
